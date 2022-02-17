4328323404c66f8f6586ece8bea2e1ffe60ddc08
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-17 10:57:47 +0200

    Complete exercise P2-02

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,10 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        double root = Math.sqrt(first + second);
+        System.out.println(root);
 
     }
 }
