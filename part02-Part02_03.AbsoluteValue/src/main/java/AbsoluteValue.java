cd467f420320a3860470e0675f40a5abe612f266
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-18 13:31:39 +0200

    Complete exercise P02-03

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,12 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+        if (inputNumber < 0) {
+            System.out.println(inputNumber * -1);
+        } else {
+            System.out.println(inputNumber);
+        }
 
     }
 }
