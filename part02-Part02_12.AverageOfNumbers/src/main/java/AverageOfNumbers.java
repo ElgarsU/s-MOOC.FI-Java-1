acb8368c4a94b7e1392cbaf3a6884a00f6c2688a
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-18 15:42:42 +0200

    Complete exercise P2-12

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,17 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        double sum = 0;
+        double count = 0;
+        while(true) {
+            System.out.println("Give a number:");
+            double input = Double.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            sum += input;
+            count++;
+        }
+        System.out.println("Average of the numbers: "+(sum/count));
     }
 }
