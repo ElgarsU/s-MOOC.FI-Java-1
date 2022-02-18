f612bc73fab27253b55eb5d1c92a9543c682466f
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-18 15:08:26 +0200

    Complete exercise P2-08

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,15 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int counter = 0;
+        while(true) {
+            System.out.println("Give a number:");
+            int input = scanner.nextInt();
+            if (input == 0) {
+                break;
+            }
+            counter++;
+        }
+        System.out.println("Number of numbers: "+counter);
     }
 }
