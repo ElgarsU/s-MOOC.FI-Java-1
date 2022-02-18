ccf3d5fe3a1de65783a62c15f63502ca098b046f
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-18 14:39:42 +0200

    Complete exercise P2-06

--- part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -5,6 +5,12 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            System.out.println("Give a number:");
+            int key = scanner.nextInt();
+            if (key == 4) {
+                break;
+            }
+        }    
     }
 }
