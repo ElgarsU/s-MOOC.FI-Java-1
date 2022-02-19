0c01716a1d8dd05f71247c4507f5197fcf0fe524
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-19 12:02:02 +0200

    Complete exercise P2-16

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -5,7 +5,15 @@ public class FromWhereToWhere {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+        System.out.println("Where to?");
+        int endNumber = scanner.nextInt();
+        System.out.println("Where from?");
+        int startNumber = scanner.nextInt();
+        for (int i = startNumber; i <= endNumber; i++) {
+            if (startNumber > endNumber) {
+                break;
+            }
+            System.out.println(i);
+        }
     }
 }
