631b22a1cbef46a9ab2b3ebd6e41cd0adb296b29
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-19 19:03:55 +0200

    Complete exercise P2-31

--- part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -2,9 +2,10 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-        //write some code here
-
-        return -1;
+        int greatest;
+        greatest = (number1 > number2) ? number1 : number2;
+        greatest = (number3 > greatest) ? number3 : greatest;
+        return greatest;
     }
 
     public static void main(String[] args) {
