f4d2b471350554eb18e16e288ec3f003b999bfed
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-19 13:56:29 +0200

    Complete exercise P2-20

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -13,6 +13,32 @@ public class RepeatingBreakingAndRemembering {
         // the parts you haven't done, but you'll get points for the finished parts.
         
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Give numbers:");
+        int sum = 0;
+        int numberCount = 0;
+        double average;
+        int even = 0;
+        int odd = 0;
+        while (true) {
+            int inputNumber = Integer.valueOf(scanner.nextLine());
+            if (inputNumber < 0) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+            if (inputNumber % 2 == 0) {
+                even++;
+            }
+            if (inputNumber % 2 != 0) {
+                odd++;
+            }
+            sum += inputNumber;
+            numberCount++;
+        }
+        average = (double)sum / numberCount;
+        System.out.println("Sum: "+sum);
+        System.out.println("Numbers: "+numberCount);
+        System.out.println("Average: "+average);
+        System.out.println("Even: "+even);
+        System.out.println("Odd: "+odd);
     }
 }
