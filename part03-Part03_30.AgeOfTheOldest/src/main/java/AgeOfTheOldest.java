465829d1dd5b5c349ced04b30f86b1145486d02a
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 21:19:54 +0200

    Complete exercise P3-30

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,16 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int oldestAge = 0;
+        while (true) {
+            String csv = scanner.nextLine();
+            if (csv.equals("")) {
+                break;
+            }
+            String[] splitCsv = csv.split(",");
+            int age = Integer.valueOf(splitCsv[1]);
+            oldestAge = (age > oldestAge) ? age : oldestAge;
+        }
+        System.out.println("Age of the oldest: "+oldestAge);
     }
 }
