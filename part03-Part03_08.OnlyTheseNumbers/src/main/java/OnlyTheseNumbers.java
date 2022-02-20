82abb98433c456702f09dd0033791cadc4a90a80
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 11:21:12 +0200

    Complete exercise P3-08

--- part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
+++ part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
@@ -16,6 +16,12 @@ public class OnlyTheseNumbers {
 
             numbers.add(number);
         }
-
+        System.out.println("From where? ");
+        int start = scanner.nextInt();
+        System.out.println("To wher? ");
+        int end = scanner.nextInt();
+        for (int i = start; i <= end; i++) {
+            System.out.println(numbers.get(i));
+        }
     }
 }
