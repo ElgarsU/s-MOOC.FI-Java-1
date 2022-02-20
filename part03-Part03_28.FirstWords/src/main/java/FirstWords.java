7e13765d22677637c99594e2f3892093b71209b0
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 21:06:18 +0200

    Complete exercise P3-28

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,11 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        for (int i = 0; i < 3; i++) {
+            String text = scanner.nextLine();
+            String[] splitText = text.split(" ");
+            System.out.println(splitText[0]);
+        }
 
     }
 }
