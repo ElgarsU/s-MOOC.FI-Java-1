c4a55634b6444f39b736cecc8a2bfd4addeea0fa
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 21:08:06 +0200

    Complete exercise P3-29

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,11 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        for (int i = 0; i < 3; i++) {
+            String text = scanner.nextLine();
+            String[] splitText = text.split(" ");
+            System.out.println(splitText[splitText.length-1]);
+        }
 
     }
 }
