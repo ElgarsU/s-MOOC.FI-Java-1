77d2ed6f83a0b2082b49aa202f530ba2ffee48f4
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 21:02:14 +0200

    Complete exercise P3-27

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,14 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        for (int i = 0; i < 3; i++) {
+            String text = scanner.nextLine();
+            String[] split = text.split(" ");
+            for (int j = 0; j < split.length; j++) {
+                if (split[j].contains("av")) {
+                    System.out.println(split[j]);
+                }
+            }
+        }
     }
 }
