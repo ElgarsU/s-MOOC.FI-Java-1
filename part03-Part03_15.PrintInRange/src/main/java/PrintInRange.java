b3209407a7d738d4b543627b4ec76a41701c98cb
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 15:58:23 +0200

    Complete exercise P3-15

--- part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
+++ part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
@@ -3,6 +3,14 @@ import java.util.ArrayList;
 
 public class PrintInRange {
 
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        System.out.println("The numbers in the range ["+lowerLimit+", "+upperLimit+"]");
+        for (int element : numbers) {
+            if (element > lowerLimit && element < upperLimit) {
+                System.out.println(element);
+            }
+        }
+    }
     public static void main(String[] args) {
         // Try your method here
     }
