1955140cde70bba70354428c6cf3680451603f28
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 21:31:42 +0200

    Complete exercise P3-31

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,21 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int oldestAge = 0;
+        String oldestName = "";
+        while (true) {
+            String csv = scanner.nextLine();
+            if (csv.equals("")) {
+                break;
+            }
+            String[] splitCsv = csv.split(",");
+            int age = Integer.valueOf(splitCsv[1]);
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestName = splitCsv[0];
+            }
+        }
+        System.out.println("Name of the oldest: "+oldestName);
 
     }
 }
