9be4328246222ca92f78c585ac85885a33b58a8a
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 20:54:23 +0200

    Complete exercise P3-26

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,15 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+            while (true) {
+                String text = scanner.nextLine();
+                if (text.equals("")) {
+                    break;
+                }
+                String[] word = text.split(" ");
+                for (int i = 0; i < word.length; i++) {
+                    System.out.println(word[i]);
+            }
+        }
     }
 }
