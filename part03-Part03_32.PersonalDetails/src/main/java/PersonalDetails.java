72706f22299532770a33fcd7a63f1013ce75444a
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-20 21:43:45 +0200

    Complete exercise P3-32

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,19 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        String longestName = "";
+        int yearSum = 0;
+        double counter = 0;
+        while (true) {
+            String csv = scanner.nextLine();
+            if (csv.equals("")) break;
+            String[] splitCSV = csv.split(",");
+            int age = Integer.valueOf(splitCSV[1]);
+            longestName = (longestName.length() < splitCSV[0].length()) ? splitCSV[0] : longestName;
+            yearSum += age;
+            counter++;
+        }
+        System.out.println("Longest name: "+longestName);
+        System.out.println("Average of the birth years: "+(yearSum/counter));
     }
 }
