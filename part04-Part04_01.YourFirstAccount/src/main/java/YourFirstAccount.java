04238e1b7edeef9bee4b9d1b88d06a24ad53726f
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-21 08:41:22 +0200

    Complete exercise P4-01

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account firstAccount = new Account("New account", 100.00);
+        firstAccount.deposit(20);
+        System.out.println(firstAccount.toString());
     }
 }
