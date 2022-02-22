88c3b922717bce6a8bb3a941b224f116e2a94ae3
Elgars Upitis <elgars.upitis@gmail.com>
2022-02-22 11:49:26 +0200

    File initialization

---
+++ part04-Part04_09.Debt/src/main/java/Debt.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author ElgarsU
+ */
+public class Debt {
+    private double balance;
+    private double interesRate;
+    
+    public Debt(double initialBalance, double initialInterestRate) {
+        this.balance = initialBalance;
+        this.interesRate = initialInterestRate;
+    }
+    
+    public void printBalance() {
+        System.out.println(this.balance);
+    }
+    
+    public void waitOneYear() {
+        this.balance = this.balance * this.interesRate;
+    }
+}
