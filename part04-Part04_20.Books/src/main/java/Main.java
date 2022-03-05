78bee09f042077d58a879a937be9ca58a4cc9c4a
Elgars Upitis <elgars.upitis@gmail.com>
2022-03-05 16:59:29 +0200

    Complete exercise P4-20

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,37 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
+        
+        Scanner input = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
+        
+        while (true) {
+            System.out.println("Title: ");
+            String title = input.nextLine();
+            if (title.isEmpty()) {
+                break;
+            }
+            System.out.println("Pages: ");
+            int pages = Integer.valueOf(input.nextLine());
+            System.out.println("Year: ");
+            int year = Integer.valueOf(input.nextLine());
+            
+            books.add(new Book(title, pages, year));
+        }
+        
+        System.out.println("What information will be printed?");
+        String output = input.nextLine();
+        
+        if (output.equals("everything")) {
+            for (Book book : books) {
+                System.out.println(book.toString());
+            }
+        }
+        if (output.equals("name")) {
+            for (Book book : books) {
+                System.out.println(book.getName());
+            }
+        }
 
     }
 }
