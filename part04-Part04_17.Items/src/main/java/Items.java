dfdd52413bd69948197abdfb1ad7ce8e03d777a6
Elgars Upitis <elgars.upitis@gmail.com>
2022-03-05 15:13:14 +0200

    Complete exercise P4-17

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -9,6 +9,18 @@ public class Items {
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            System.out.print("Name: ");
+            String itemName = scanner.nextLine();
+            if (itemName.isEmpty()) {
+                break;
+            }
+            items.add(new Item(itemName));
+        }
+        
+        for (Item item : items) {
+            System.out.println(item.toString());
+        }
     }
 }
