19f2ba6c5ce0064ddbbcd7925a808a278c9088d2
Elgars Upitis <elgars.upitis@gmail.com>
2022-03-23 11:41:49 +0200

    Complete exercise P4-29

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,23 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Name of the file:");
+        String fileName = scanner.nextLine();
+        
+        try (Scanner file = new Scanner(Paths.get(fileName))) {
+            
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
+                
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                
+                System.out.print(name+", age: "+age);
+                System.out.println((age==1)?" year":" years");
+            }
+        } catch (Exception e) {
+            System.out.println("");
+        }
     }
 }
