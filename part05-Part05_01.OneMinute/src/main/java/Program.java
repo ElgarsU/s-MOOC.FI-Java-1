45a8e42b648f5d604d23f4652b0358019f591308
Elgars Upitis <elgars.upitis@gmail.com>
2022-03-29 13:43:56 +0300

    Complete exercise P5-01

--- part05-Part05_01.OneMinute/src/main/java/Program.java
+++ part05-Part05_01.OneMinute/src/main/java/Program.java
@@ -5,7 +5,13 @@ public class Program {
 
     public static void main(String[] args) {
         // You can test your program here
-
-
+        
+        Timer t = new Timer();
+        int i = 0;
+        while (i < 362958) {
+            t.advance();
+            i++;
+        }  
+        System.out.println(t);
     }
 }
