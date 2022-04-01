a3724dd00a1bfb792f905693b1b077b6c5c61631
Elgars Upitis <elgars.upitis@gmail.com>
2022-04-01 12:09:20 +0300

    Complete exercise P5-11

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,22 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
+    
+    public boolean largerThan(Apartment compared) {
+        return (this.squares > compared.squares);
+    }
+    
+    public int priceDifference(Apartment compared) {
+        int apartmentPrice = this.squares * this.princePerSquare;
+        int comparedPrice = compared.squares * compared.princePerSquare;
+        int difference = apartmentPrice - comparedPrice;
+        return (difference > 0) ? difference : difference * -1;
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared) {
+        int apartmentPrice = this.squares * this.princePerSquare;
+        int comparedPrice = compared.squares * compared.princePerSquare;
+        return (apartmentPrice > comparedPrice);
+    }
 
 }
